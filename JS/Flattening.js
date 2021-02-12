68b619776d65f09e3dbb89ab851226b4778e3e4c
Juanes-GH <jjgohe3@gmail.com>
2021-02-12 11:48:04 +0100

    Ex. switch

---
+++ JS/Flattening.js
@@ -0,0 +1,38 @@
+var notas =10 ;
+switch(notas){
+    case 0:
+        console.log('muy insuficiente');
+        break;
+    case 1:
+        console.log('muy insuficiente');
+        break;
+    case 2:
+        console.log('muy insuficiente');
+        break;
+    case 3:
+        console.log('muy insuficiente');
+        break;
+    case  4:
+        console.log('insuficiente');
+        break;
+    case  5:
+        console.log('suficiente');
+        break;
+    case  6:
+        console.log('bien');
+        break;
+    case  7 :
+        console.log('notable');
+        break;
+    case  8 :
+        console.log('notable');
+        break;
+    case  9 :
+        console.log('sb');
+        break;
+    case  10 :
+            console.log('Msb');
+            break;
+    default:
+        console.log('nota no valida');
+}
