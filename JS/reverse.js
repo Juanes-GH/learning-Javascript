8deabc3a7e32a3f54a83a33f80a5ec88fa3b97f5
Juanes-GH <jjgohe3@gmail.com>
2021-02-12 12:02:18 +0100

    Ex.reverse

---
+++ JS/reverse.js
@@ -0,0 +1,12 @@
+var reverseArray = [1, 2,3]
+function reverseArray(){
+  console.log(reverseArray)
+};
+reverseArray.reverse();
+console.log(reverseArray);
+var reverseArrayInPLace = [3,2,1]
+function reverseArrayInPLace(){
+  console.log(reverseArrayInPLace)
+};
+reverseArrayInPLace.reverse();
+console.log(reverseArrayInPLace)
