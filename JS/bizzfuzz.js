f8bff3f7b1c782c0f714176de624841504a1f982
Juanes-GH <jjgohe3@gmail.com>
2021-02-12 12:10:29 +0100

    Ex.BIZFUZZ

---
+++ JS/bizzfuzz.js
@@ -0,0 +1,10 @@
+for(let i = 0; i<= 100; i++){
+    if(i % 3){
+        console.log('FUZZ');
+    }
+    if(i % 5){
+        console.log('BUZZ');
+    }else if(i% 3,5){
+        console.log('BIZZFUZZ');
+    }
+}
