7a0a689c5746c7f55689bac65beb2609970c86da
Juanes-GH <jjgohe3@gmail.com>
2021-02-16 11:09:03 +0100

    Flatten

--- JS/GitExamles.js/flatten.js
+++ JS/GitExamles.js/flatten.js
@@ -1,3 +1,4 @@
+/* In this taks I have to use the reduce method in combination the concat method to "flaten" an array of array into a simgle array that has all the elements of the original arrays*/
 let a = [2,4,8,6]
 let b = [5,5,3,7]
 let c = a.concat(b);
