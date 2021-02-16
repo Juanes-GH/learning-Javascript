d02d3af494962b8974989b66e3fe58ea3158a449
Juanes-GH <jjgohe3@gmail.com>
2021-02-16 11:03:36 +0100

    Chess

--- JS/GitExamles.js/Chess.js
+++ JS/GitExamles.js/Chess.js
@@ -1,5 +1,6 @@
-let chessboard = '# # # #'
-let chessboard_2 = ' # # # #'
+/* In this taks I have to cretes a string that represents an 8 X 8 grid*/
+let chessboard = '# # # # # # # #'
+let chessboard_2 = ' # # # # # # # #'
 for(let i= 0; i <= 3; i ++){
   console.log(chessboard)
   console.log(chessboard_2)
